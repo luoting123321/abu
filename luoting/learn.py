12f3a5763882c5bfd08c97854a0bed4bf2a0aba2
luoting01 <luoting01@mail.jj.cn>
2024-11-13 10:41:04 +0800

    测试代码

---
+++ luoting/learn.py
@@ -0,0 +1,53 @@
+from __future__ import print_function
+from __future__ import division
+
+import warnings
+warnings.filterwarnings('ignore')
+warnings.simplefilter('ignore')
+    
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+# %matplotlib inline
+import os
+import sys
+# 使用insert 0即只使用github，避免交叉使用了pip安装的abupy，导致的版本不一致问题
+if sys.path[0] != os.path.abspath('./'):
+    sys.path.insert(0, os.path.abspath('./'))
+# for path in sys.path:
+#     print(path)
+import abupy
+
+# abupy.env.enable_example_env_ipython()
+
+# from abupy import AbuFactorBuyXD, BuyCallMixin
+
+# class AbuFactorBuyBreak(AbuFactorBuyXD, BuyCallMixin):
+#     """示例继承AbuFactorBuyXD完成正向突破买入择时类, 混入BuyCallMixin，即向上突破触发买入event"""
+#     def fit_day(self, today):
+#         """
+#         针对每一个交易日拟合买入交易策略，寻找向上突破买入机会
+#         :param today: 当前驱动的交易日金融时间序列数据
+#         :return:
+#         """
+#         # 今天的收盘价格达到xd天内最高价格则符合买入条件
+#         if today.close == self.xd_kl.close.max():
+#             # 生成买入订单, 由于使用了今天的收盘价格做为策略信号判断，所以信号发出后，只能明天买
+#             return self.buy_tomorrow()
+#         return None
+    
+# from abupy import AbuBenchmark
+# from abupy import AbuCapital
+
+# # buy_factors 60日向上突破，42日向上突破两个因子
+# buy_factors = [{'xd': 60, 'class': AbuFactorBuyBreak}, 
+#                {'xd': 42, 'class': AbuFactorBuyBreak}]
+# benchmark = AbuBenchmark()
+# capital = AbuCapital(1000000, benchmark)
+
+# from abupy import ABuPickTimeExecute
+# orders_pd, action_pd, _ = ABuPickTimeExecute.do_symbols_with_same_factors(['usTSLA'],
+#                                                                             benchmark,
+#                                                                             buy_factors,
+#                                                                             None,
+#                                                                             capital, show=True)
